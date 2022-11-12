1154113845cfefec85d78d945f555ef6c91da54b
Sxmxrth <samarthgupta1202@gmail.com>
2022-11-12 09:54:04 +0530

    website added

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("home", {
+        content : "Hello there"
+    })
+})
+
+app.get("/client", function(req, res) {
+    res.render("client")
+})
+
+app.get("/agent", function(req, res) {
+    res.render("agent")
+})
+
+app.post("/client", function(req, res){
+    let email = req.body.email;
+    let pwd = req.body.pwd;
+
+    console.log(email);
+
+    if(email == "samarth@gmail.com"){
+        res.render("success")
+    }
+    else{
+        console.log("failure");
+    }
+
+})
+
+app.listen(3000, function(){
+    console.log("server is up and running on port 3000");
+})
